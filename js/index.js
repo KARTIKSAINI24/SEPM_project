0314da271a5f8d8b5a561f50ae1bf9bbfd87c815
KARTIKSAINI24 <sainikartik24@gmail.com>
2021-04-07 23:08:12 +0530

    second

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+// var Typed = require('typed.min.js');
+
+// let typed = new Typed(".typing", {
+//     strings: ["DEHRADUN", "This typing animation is very easy.", "You can easily make animation like this.", "And don't forget to Follow Me.","Thanks!"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true,
+//   });
+
+// .btn-book{
+//   addEventListener()
+// }
