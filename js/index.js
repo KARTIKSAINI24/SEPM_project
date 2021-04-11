8550e2e143cd3f591bbf5f0922a105bacdef6f4a
KARTIKSAINI24 <sainikartik24@gmail.com>
2021-04-12 00:11:01 +0530

    sign-up-form added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,52 @@
 let typed = new Typed(".typing_locations", {
-    strings: ["Chennai?", "Mumbai?", "Dehradun?", "New Delhi?","Gujrat?","Uttarakhand","Goa","Varanasi"],
+    strings: ["Chennai?", "Mumbai?", "Dehradun?", "New Delhi?","Gujrat?","Uttarakhand?","Goa?","Varanasi?"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
+// ************************************** creating modal *****************************
+
+// var signup_modal = document.querySelector("#signup-modal")
+
+// document.getElementById("sign-up-Btn").onclick = function(){
+    
+//     console.log('sign up')
+//     signup_modal.style.display = "none";
+
+// }
+
+// var modal_card = document.querySelector(".row")
+
+// window.onclick = function(event) {
+//     console.log("out")
+//    if (event.target == signup_modal) {
+//      signup_modal.style.display = "none";
+//    }
+// }
+
+//   // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("sign-up-Btn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// } 
